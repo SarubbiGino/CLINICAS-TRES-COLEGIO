70bb1bf6a0764c628e9fcf27ab85b04301221629
Gino Sarubbi <110644113+SarubbiGino@users.noreply.github.com>
2022-09-24 15:14:55 -0300

    Add files via upload

--- src/components/tituloMain/tituloMain.jsx
+++ src/components/tituloMain/tituloMain.jsx
@@ -27,7 +27,8 @@ export default function tituloMain() {
          className={estilos.contenedorTituloPButton}>
    
          <Grid item xs={0}><h1 className={estilos.titulo}>Donacion de sangre</h1></Grid>
-         <Grid item xs={0}><p className={estilos.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, est necessitatibus voluptates iusto labore architecto temporibus dolorum, harum fugiat magni error pariatur ipsa! Fugit nesciunt</p></Grid>
+         <Grid item xs={0}><p className={estilos.p}>Personas entre 16 y 65 años que gocen de buena salud y pesen más de 50 kilos.
+Quienes califiquen en la entrevista y examen clínico <a href='#' className={estilos.linkalFormulario}>(podes visualizarlo aquí)</a>. Aclaración: el mismo no tiene que ser llevado, se lo darán en el establecimiento.</p></Grid>
          <Grid item xs={0} className="contenedorBTN"><button className={estilos.Button}>Formulario de donacion</button></Grid>
       </Grid>
         <img className={estilos.IMG} src="https://static.wixstatic.com/media/d45da4_4a6e56a63f804b8db801f166bb921e17~mv2.png/v1/fill/w_676,h_658,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/wix.png" alt="" />
