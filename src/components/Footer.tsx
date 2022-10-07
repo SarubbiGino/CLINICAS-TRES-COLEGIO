9e0bc23151448d2910ef7e53d99620ba08dce3ea
Gino Sarubbi <g.sarubbi.17@etec.uba.ar>
2022-10-07 14:37:42 -0300

    nuevo home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,22 +17,23 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Footer() {
   return (
-  <Box className={estilos.footer}>
-    <Container maxWidth = "lg">
-        <Grid container spacing = {5} className={estilos.contenedorFooter} direction="column" alignItems="center" justifyContent="center">
-            <Grid item xs ={12} sm={4} className={estilos.contenedoRedesSociales} >
+    <Box className={estilos.footer}>
+      <Container maxWidth="lg">
+        <Grid container spacing={5} className={estilos.contenedorFooter} direction="column" alignItems="center" justifyContent="center">
+          <Grid item xs={12} sm={4} className={estilos.contenedoRedesSociales} >
 
-             <Box className={estilos.redes}>Redes Sociales</Box>
-                <Box className={estilos.contenedorLogos} >
-                   <FacebookIcon className={estilos.logos}/>
-                   <TwitterIcon className={estilos.logos}/>
-                   <LinkedInIcon className={estilos.logos}/>
-                   <WhatsappIcon className={estilos.logos}/> 
-                </Box>
-            </Grid>
-            <Box className={estilos.clinicas}   alignItems="center" justifyContent="center" textAlign="center">HOSPITAL DE CLÍNICAS "JOSÉ DE SAN MARTÍN" All Rights Reserved</Box>
+            <Box className={estilos.redes}>Redes Sociales</Box>
+            <Box className={estilos.contenedorLogos} >
+
+              <a href="https://m.facebook.com/profile.php?id=110189185672713"><FacebookIcon className={estilos.logos} /></a>
+              <a href="https://twitter.com/Prensaclinicas?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><TwitterIcon className={estilos.logos} /></a>
+              <a href="https://www.instagram.com/hospitaldeclinicasok/?hl=es">  <InstagramIcon className={estilos.logos} /> </a>
+              <WhatsappIcon className={estilos.logos} />
+            </Box>
+          </Grid>
+          <Box className={estilos.clinicas} alignItems="center" justifyContent="center" textAlign="center">HOSPITAL DE CLÍNICAS "JOSÉ DE SAN MARTÍN" All Rights Reserved</Box>
         </Grid>
-    </Container>
-  </Box>
+      </Container>
+    </Box>
   );
 }
